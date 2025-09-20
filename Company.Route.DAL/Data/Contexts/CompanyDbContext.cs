e309213a92b845426b00bbc3881b9298cbc05bad
HFCS <HFCS@DESKTOP-VUEEAH9>
2025-09-20 19:46:10 +0300

    Implementation Department Module DAL

---
+++ Company.Route.DAL/Data/Contexts/CompanyDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Company.Route.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Company.Route.DAL.Data.Contexts
+{
+    internal class CompanyDbContext : DbContext
+    {
+        public CompanyDbContext() : base()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Server=.; DataBase=CompanyApp; Trusted_Connection=True; TrustServerCertificate=True");
+
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ Company.Route.DAL/Models/Department.cs
@@ -0,0 +1,11 @@
+﻿namespace Company.Route.DAL.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateAt { get; set; }
+
+    }
+}
