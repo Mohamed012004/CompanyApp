a74ff5e8e140b44dd5357e1eb6ef4bd4bae1f690
HFCS <HFCS@DESKTOP-VUEEAH9>
2025-09-26 21:18:50 +0300

    Implementation Of Employee Module DAL

---
+++ Company.Route.DAL/Data/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Company.Route.DAL.Data.Configurations
+{
+    internal class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.Property(E => E.Salary).HasColumnType("decimal(18,2)");
+        }
+    }
+}
---
+++ Company.Route.DAL/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace Company.Route.DAL
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public double Age { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public decimal Salary { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public DateTime HiringDate { get; set; }
+        public DateTime CreateAt { get; set; }
+
+
+    }
+}
